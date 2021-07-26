622fa3e0f54952e1c049e02b9b5aae64e78e9d3f
Ian <ianbergia.rc@gmail.com>
2021-07-26 02:03:43 -0300

    interval input added

--- src/components/interval-input/IntervalInput.tsx
+++ src/components/interval-input/IntervalInput.tsx
@@ -1,8 +1,16 @@
-const IntervalInput = () => {
+interface InIntervalInput {
+    speed: number;
+    handleChangeInterval: (e: any) => void;
+}
+const IntervalInput = ({speed, handleChangeInterval}: InIntervalInput) => {
     return (
-        <div className="">
-        <label htmlFor="interval">Intervalo en ms</label>
-        <input type="number" id="interval" onChange={() => console.log('xd')}/>
+        <div className="interval__container">
+            <label htmlFor="interval" className="interval__container-label">Intervalo en ms</label>
+            <input type="number" id="interval"
+            defaultValue={speed}
+            className="interval__container-input"
+             onChange={handleChangeInterval}
+             />
         </div>  
     )
 }
